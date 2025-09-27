29aebae494abaa74d17a868116c917e49324b7a8
JurajNznsk <juraj.niznansky@gmail.com>
2025-09-27 19:17:56 +0200

    Basic CLI instructions: ADDED; Task Class: ADDED

--- Main.py
+++ Main.py
@@ -1,12 +1,20 @@
-from tkinter import *
+#todo
+"""
+Add, Update, and Delete tasks
+Mark a task as in progress or done
+List all tasks
+List all tasks that are done
+List all tasks that are not done
+List all tasks that are in progress
 
-root = Tk()
-
-canvas = Canvas(root, width = 500, height=500)
-canvas.pack()
-
-mainloop()
-
+Store to JSON
+"""
+print("****************************")
+print("What would you like to do?: ")
+print("1. Add task")
+print("2. Update task")
+print("3. Delete task")
+print("****************************")
 
 
 
---
+++ Task.py
@@ -0,0 +1,4 @@
+
+
+class Task:
+    id = 1
