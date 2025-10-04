edff2324173c28a2a8a0e4e3ee1df2adcaca8c23
JurajNznsk <juraj.niznansky@gmail.com>
2025-10-04 11:26:44 +0200

    Task.py: class structure for tasks instances added

--- Main.py
+++ Main.py
@@ -9,6 +9,18 @@ List all tasks that are in progress
 
 Store to JSON
 """
+from Task import *
+
+tasks = [
+    Task("MAS", "Dve predavacky", "so 4.10.2025", "15:00"),
+    Task("SI", "Interview", "po 6.10.2025", "13:00", Progress.IN_PROGRESS)
+]
+
+for task in tasks:
+    print("________________________")
+    task.print_task()
+    print("________________________")
+
 print("****************************")
 print("What would you like to do?: ")
 print("1. Add task")
@@ -16,5 +28,7 @@ print("2. Update task")
 print("3. Delete task")
 print("****************************")
 
-
+while True:
+    x = str(input("Make a choice: "))
+    print("Your choice was " + x + ".")
 
--- Task.py
+++ Task.py
@@ -1,4 +1,19 @@
+from enum import Enum, auto
 
+class Progress(Enum):
+    DONE = auto(),
+    IN_PROGRESS = auto(),
+    NOT_DONE = auto()
 
 class Task:
-    id = 1
+    def __init__(self, acronym, description, date, time, progress = Progress.NOT_DONE):
+        self.acronym = acronym.upper().ljust(3)[:3]
+        self.description = description
+        self.date = date
+        self.time = time
+        self.progress = progress
+
+    def print_task(self):
+        print(f"{self.acronym} - {self.description}")
+        print(f"{self.date}; {self.time}")
+        print(f"{self.progress.name}")
